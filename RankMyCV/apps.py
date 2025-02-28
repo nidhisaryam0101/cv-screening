6f84692324078a9ce4f5e79e1a2225c439860cf7
nidhisaryam0101 <nidhisaryam0101@gmail.com>
2025-02-28 14:49:04 +0530

    Delete RankMyCV directory

--- RankMyCV/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HrInterfaceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hr_interface'
--- RankMyCV/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.job_description, name='job_description'),
-]
